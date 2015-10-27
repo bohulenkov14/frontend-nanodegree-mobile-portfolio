c518e45415a7589d977a9504859b9d4f68a0d272
bohulenkov.a <bohulenkov14@gmail.com>
2015-10-27 18:12:15 +0300

    feat: Added gulp authomatization and site rendering optimiztions
    -Added gulp file that executes minimization and inlining of css and js files for index.html and it's dependencies
    -Gulp file copies contents of /view folder additionally for folder structure consistency
    -Added rendering optimizations for pizza site: optimized painting and layout changes for moving pizzas and optimized slider rendering

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,106 @@ var gulp = require('gulp');
 var gm = require('gulp-gm');
 var imagemin = require('gulp-imagemin');
 var imageResize = require('gulp-image-resize');
+var inlinesource = require('gulp-inline-source');
+var minifyHTML = require('gulp-minify-html');
+var minifyCss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 
-gulp.task('jpgcompress', function() {
-  return gulp.src('views/images/**.jpg')
-      .pipe(imageResize({
-        width: 100,
-        imageMagick: true
-      }))
-      .pipe(gm(function(gmfile) { return gmfile; }, { imageMagick: true }))
-      .pipe(imagemin({
-        progressive: true
-      }))
-      .pipe(gulp.dest('views/dist/images'));
+
+
+//=========================================
+//Resume site tasks
+//=========================================
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(minifyCss({compatibility: 'ie8'}))
+    .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('default', ['jpgcompress']);
+gulp.task('uglify-js', function() {
+  return gulp.src('js/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js'));
+});
+
+gulp.task('inlinesource', function () {
+  return gulp.src('*.html')
+    .pipe(inlinesource())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('jpgCompress', function() {
+  return gulp.src('img/**.jpg')
+    .pipe(gm(function(gmfile) { return gmfile; }, { imageMagick: true }))
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('build/img'));
+});
+
+gulp.task('jpgResizeCompress', function() {
+  return gulp.src('views/images/**.jpg')
+    .pipe(imageResize({
+      width: 100,
+      imageMagick: true
+    }))
+    .pipe(gm(function(gmfile) { return gmfile; }, { imageMagick: true }))
+    .pipe(imagemin({
+      progressive: true
+    }))
+    .pipe(gulp.dest('build/views/images'));
+});
+
+gulp.task('minify-html', function() {
+  return gulp.src('build/*.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('resume',
+  [
+    'minify-css',
+    'uglify-js',
+    'jpgResizeCompress',
+    'jpgCompress',
+    'inlinesource',
+    'minify-html'
+  ]
+);
+
+//=========================================
+//Pizza site tasks
+//=========================================
+
+gulp.task('copy-css', function() {
+  return gulp.src('views/css/*.css')
+    .pipe(gulp.dest('build/views/css'));
+});
+
+gulp.task('copy-js', function() {
+  return gulp.src('views/js/*.js')
+    .pipe(gulp.dest('build/views/js'));
+});
+
+gulp.task('copy-html', function () {
+  return gulp.src('views/*.html')
+    .pipe(gulp.dest('build/views/'));
+});
+
+gulp.task('copy-png', function () {
+  return gulp.src('views/images/*.png')
+    .pipe(gulp.dest('build/views/images'));
+});
+
+gulp.task('pizza',
+  [
+    'copy-css',
+    'copy-js',
+    'copy-html',
+    'copy-png',
+  ]
+);
+
+
+gulp.task('default', ['resume', 'pizza']);
